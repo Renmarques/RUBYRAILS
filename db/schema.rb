0dc0170e05e9f8bb56964f68577490476a1c76ed
ED25519 <Renanmoreiramarques@gmail.com>
2023-03-08 19:36:17 -0300

    atualização do projeto

--- db/schema.rb
+++ db/schema.rb
@@ -14,20 +14,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_184124) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "produtcs", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
